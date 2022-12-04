ed6055e72119263f38219a6c604ca2f1f8e04d38
Ali Darvish <ali.darvish.sf@gmail.com>
2022-12-05 02:28:24 +0330

    Solve 1-2

--- javaScriptExercise/1-2/1-2.js
+++ javaScriptExercise/1-2/1-2.js
@@ -1,16 +1,24 @@
-// Find array depth
-function getArrayDepth(input) {
-  return Array.isArray(input)
-    ? 1 + Math.max(0, ...input.map(getArrayDepth))
-    : 0;
-}
+// Flat without Flat Method
 
-function arrayDepth(inputArray) {
-  if (!arrayValidator(inputArray)) {
-    return new Error(`Input must be an array`);
+function flatten(input) {
+  let result = [];
+  for (let i = 0; i < input.length; i++) {
+    let item = input[i];
+    if (!Array.isArray(item)) {
+      result.push(item);
+    } else {
+      result = result.concat(flatten(item));
+    }
   }
-  return getArrayDepth(inputArray);
+  return result;
 }
 
-let testArray = [1, 2, [3, 4, [5, 6], 7, [8, [9, 91]], 10], 11, 12];
-console.log(arrayDepth(testArray));
+function flatArray(inputArray) {
+  if (!arrayValidator(inputArray)) {
+    return new Error(`Input must be an array!`);
+  }
+  return flatten(inputArray);
+}
+
+let testArray = [1, [2], [3, [4, [5, 6]], [[7]], [8, [[[9]]], 10], 11, 12]];
+console.log(flatArray(testArray));
